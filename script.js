28b884eb7c12a98da61fafa8ae0a34dcf190a93a
hugoresende27 <Hugoresende27@gmail.com>
2021-11-18 20:14:25 +0000

    s

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const addBtn = document.getElementById('add')
 
-addBtn.addEventListener('click', ()=> addNovaNota())
+const notas = JSON.parse(localStorage.getItem('notas')) //para ir buscar o array notas ao localStorage, JSON.parse para vir no formato array e não string
+console.log(notas)
+
+if (notas) {//se existirem notas no localStorage, loop forEach com função addNovaNota com o parametro nota
+    notas.forEach( nota => addNovaNota(nota))
+}
+addBtn.addEventListener('click', () => addNovaNota())
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -23,22 +29,42 @@ function addNovaNota ( text = '') {
     const editBtn = note.querySelector('.edit')
     const deleteBtn = note.querySelector('.delete')
     const main = note.querySelector('.main')
-    const textArea = note.querySelector('.textarea')
+    const textArea = note.querySelector('textarea') //aqui textarea não leva . porque não é uma classe
+
+    textArea.value = text
+    main.innerHTML = marked(text)       //uso da biblioteca marked () para marcar o texto
 
     deleteBtn.addEventListener('click', () => {//delete das notas, função arrow com eventlistener em click, note remove()
         note.remove()
+
+        updateBN()
     })
 
-    // editBtn.addEventListener('click', () => {//delete das notas, função arrow com eventlistener em click, note remove()
-    //     main.classList.toggle('hidden')
-    //     textArea.classList.toggle('hidden')
-    // })
-
-    editBtn.addEventListener('click', () => {
+    editBtn.addEventListener('click', () => {//delete das notas, função arrow com eventlistener em click, note remove()
         main.classList.toggle('hidden')
         textArea.classList.toggle('hidden')
     })
 
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target
+
+        main.innerHTML = marked(value)
+
+        updateBN()
+    })
 
     document.body.appendChild(note)     //document.body para usar o body, adicionar uma nota a cadaa uso da função addNovaNota
 }
+
+function updateBN () {
+    const notasTexto = document.querySelectorAll('textarea')
+
+    const notas = []
+
+    notasTexto.forEach (nota => notas.push(nota.value)) //funçao arrow para fazer push no array notas de cada nota recebida como parametro
+
+   // console.log(notas )
+
+   localStorage.setItem('notas', JSON.stringify(notas))//gravar no localStorage do browser, em formato array
+}
+
